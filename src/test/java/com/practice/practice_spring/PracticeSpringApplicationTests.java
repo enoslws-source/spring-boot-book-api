c6aca5c754a90aa8785f609b889656143de4951b
enoslewis <elewi9@wgu.edu>
2026-05-20 09:05:24 -0700

    Application test fixes

--- src/test/java/com/practice/practice_spring/PracticeSpringApplicationTests.java
+++ src/test/java/com/practice/practice_spring/PracticeSpringApplicationTests.java
@@ -1,9 +1,5 @@
 package com.practice.practice_spring;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class PracticeSpringApplicationTests {
 
 
